e19b3b149c5ba5d33ddcca90b065640ed49a8e1c
bhavesh732 <bhaveshkhivesra@gmail.com>
2020-11-05 02:16:50 +0530

    Updates

---
+++ precache-manifest.0e211e0788b79bf9d460d44ba3d1f2bb.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f3564ab52db31d42a700837ced2353",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ec6edfb693aa7714627",
+    "url": "/static/css/2.3886e4c5.chunk.css"
+  },
+  {
+    "revision": "2a58cdaa2b905db937dc",
+    "url": "/static/css/main.7b4b2bb4.chunk.css"
+  },
+  {
+    "revision": "7ec6edfb693aa7714627",
+    "url": "/static/js/2.6b440473.chunk.js"
+  },
+  {
+    "revision": "caacfaae96ba8b2d6a270a80b7b8b4cb",
+    "url": "/static/js/2.6b440473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a58cdaa2b905db937dc",
+    "url": "/static/js/main.b241464b.chunk.js"
+  },
+  {
+    "revision": "c519c9a1f6268bae0e79",
+    "url": "/static/js/runtime-main.8883081a.js"
+  },
+  {
+    "revision": "50c63ebc60e4f6f502dd09b59c55146b",
+    "url": "/static/media/BebasNeue-Bold.50c63ebc.ttf"
+  },
+  {
+    "revision": "44169c432f6bc9521bdd4074971376f7",
+    "url": "/static/media/Chivo-Bold.44169c43.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "7fd4a02c1f8803151d5c7650fe9335a1",
+    "url": "/static/media/background.7fd4a02c.jpg"
+  },
+  {
+    "revision": "dc6697afdb77683a2dce069495ca4950",
+    "url": "/static/media/economics.dc6697af.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "cdd8f07e606c54b5baaccd9f9daf7bb8",
+    "url": "/static/media/logo.cdd8f07e.png"
+  },
+  {
+    "revision": "f617796f012a6a780eb25823fc9bc003",
+    "url": "/static/media/psychology.f617796f.jpg"
+  },
+  {
+    "revision": "444004758424ab22ef274899446dee99",
+    "url": "/static/media/sociology.44400475.jpg"
+  }
+]);
